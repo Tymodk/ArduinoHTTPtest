3fd86627e52682865ecbdfd569ea852229d49583
tymodk <tymo.dekock@telenet.be>
2018-12-05 12:04:27 +0100

    first commit

---
+++ app/Arduino_id.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Arduino_id extends Model
+{
+    protected $fillable = [
+        'arduino_id'
+    ];
+
+    
+}
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Arduino_id;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+    	$arduino_ids = Arduino_id::all();
+    	if(is_null($arduino_ids)){
+        	return view('welcome');
+
+    	}
+    	else{
+    		return view('welcome')->with('arduino_ids', $arduino_ids);
+    	}
+    }
+    public function store(Request $request){
+    	$arduino_id = new Arduino_id;
+    	$arduino_id->create($request);
+    }
+}
